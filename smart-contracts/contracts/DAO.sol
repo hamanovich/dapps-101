24ee3deedb66351986967473a94ce0a7e4ca10f3
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2022-12-02 20:48:34 +0300

    DAO smart contract update

--- smart-contracts/contracts/DAO.sol
+++ smart-contracts/contracts/DAO.sol
@@ -1,5 +1,19 @@
 // SPDX-License-Identifier: MIT
+
+/**
+ * DAO Contract:
+ * 1. Collects investors money
+ * 2. Keep track of investor contributions with shares
+ * 3. Allow investors to transfer shares
+ * 4. Allow investment proposals to be created and voted
+ * 5. Execute successfull investment proposals (i.e. send money)
+ */
+
 pragma solidity 0.8.17;
+
+error DAO__OnlyInvestors();
+error DAO__OnlyAdmin();
+error DAO_SendFailure();
 
 contract DAO {
     struct Proposal {
@@ -11,38 +25,38 @@ contract DAO {
         uint256 end;
         bool executed;
     }
-    mapping(address => bool) public investors;
-    mapping(address => uint256) public shares;
-    mapping(uint256 => Proposal) public proposals;
-    mapping(address => mapping(uint256 => bool)) public votes;
-    uint256 public totalShares;
-    uint256 public availableFunds;
-    uint256 public contributionEnd;
-    uint256 public nextProposalId;
-    uint256 public voteTime;
-    uint256 public quorum;
-    address public admin;
+    mapping(address => bool) private investors;
+    mapping(address => uint256) private shares;
+    mapping(uint256 => Proposal) private proposals;
+    mapping(address => mapping(uint256 => bool)) private votes;
+    uint256 private totalShares;
+    uint256 private availableFunds;
+    uint256 private contributionEnd;
+    uint256 private nextProposalId;
+    uint256 private voteTime;
+    uint256 private quorum;
+    address private admin;
 
     modifier onlyInvestors() {
-        require(investors[msg.sender] == true, "only investors");
+        if (!investors[msg.sender]) revert DAO__OnlyInvestors();
         _;
     }
 
     modifier onlyAdmin() {
-        require(msg.sender == admin, "only admin");
+        if (msg.sender != admin) revert DAO__OnlyAdmin();
         _;
     }
 
     constructor(
-        uint256 contributionTime,
+        uint256 _contributionTime,
         uint256 _voteTime,
         uint256 _quorum
     ) {
         require(
             _quorum > 0 && _quorum < 100,
-            "quorum must be between 0 and 100"
+            "quorum must be between 1 and 100"
         );
-        contributionEnd = block.timestamp + contributionTime;
+        contributionEnd = block.timestamp + _contributionTime;
         voteTime = _voteTime;
         quorum = _quorum;
         admin = msg.sender;
@@ -53,10 +67,7 @@ contract DAO {
     }
 
     function contribute() external payable {
-        require(
-            block.timestamp < contributionEnd,
-            "cannot contribute after contributionEnd"
-        );
+        require(block.timestamp < contributionEnd, "Contribution is over");
         investors[msg.sender] = true;
         shares[msg.sender] += msg.value;
         totalShares += msg.value;
@@ -69,7 +80,7 @@ contract DAO {
         shares[msg.sender] -= amount;
         availableFunds -= amount;
         (bool sent, ) = msg.sender.call{value: amount}("");
-        require(sent, "Failed to send Ether");
+        if (!sent) revert DAO_SendFailure();
     }
 
     function transferShare(uint256 amount, address to) external {
@@ -116,15 +127,12 @@ contract DAO {
         Proposal storage proposal = proposals[proposalId];
         require(
             block.timestamp >= proposal.end,
-            "cannot execute a proposal before end date"
+            "Cannot execute a proposal before end date"
         );
-        require(
-            proposal.executed == false,
-            "cannot execute a proposal already executed"
-        );
+        require(proposal.executed == false, "Proposal already executed");
         require(
             (proposal.votes / totalShares) * 100 >= quorum,
-            "cannot execute a proposal with votes below quorum"
+            "Cannot execute a proposal with votes below quorum"
         );
         _transferEther(proposal.amount, proposal.recipient);
     }
@@ -140,6 +148,34 @@ contract DAO {
         require(amount <= availableFunds, "not enough availableFunds");
         availableFunds -= amount;
         (bool sent, ) = to.call{value: amount}("");
-        require(sent, "Failed to send Ether");
+        if (!sent) revert DAO_SendFailure();
+    }
+
+    function getInvestor(address _investor) external view returns (bool) {
+        return investors[_investor];
+    }
+
+    function getInvestorShares(address _investor)
+        external
+        view
+        returns (uint256)
+    {
+        return shares[_investor];
+    }
+
+    function getProposal(uint256 _proposal)
+        external
+        view
+        returns (Proposal memory)
+    {
+        return proposals[_proposal];
+    }
+
+    function getTotalShares() external view returns (uint256) {
+        return totalShares;
+    }
+
+    function getAvailableFunds() external view returns (uint256) {
+        return availableFunds;
     }
 }
